3bc44b90904b1df0c9ebbe26179ee7ac9480b5c8
Matt Walker <matt.r.walker@gmail.com>
2013-04-26 13:10:44 -0500

    Prep for 1.0.0 release

--- lib/cascading.rb
+++ lib/cascading.rb
@@ -2,24 +2,26 @@ require 'java'
 
 module Cascading
   # :stopdoc:
-  VERSION = '0.0.10'
+  VERSION = '1.0.0'
 end
 
+require 'cascading/aggregations'
 require 'cascading/assembly'
 require 'cascading/base'
 require 'cascading/cascade'
 require 'cascading/cascading'
 require 'cascading/cascading_exception'
 require 'cascading/expr_stub'
+require 'cascading/filter_operations'
 require 'cascading/flow'
+require 'cascading/identity_operations'
 require 'cascading/mode'
 require 'cascading/operations'
-require 'cascading/identity_operations'
-require 'cascading/filter_operations'
 require 'cascading/regex_operations'
-require 'cascading/text_operations'
 require 'cascading/scope'
+require 'cascading/sub_assembly'
 require 'cascading/tap'
+require 'cascading/text_operations'
 
-# include module to make them available at top package
+# include module to make it available at top level
 include Cascading
